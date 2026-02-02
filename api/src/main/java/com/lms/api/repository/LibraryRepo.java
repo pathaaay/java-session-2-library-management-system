90649a23a67537ce58fdadcff3805980a02a95ed
pathaaay <aayush.pathak@roimaint.com>
2026-02-02 16:34:52 +0530

    first commit

--- api/src/main/java/com/lms/api/repository/LibraryRepo.java
+++ api/src/main/java/com/lms/api/repository/LibraryRepo.java
@@ -2,8 +2,12 @@ package com.lms.api.repository;
 
 import com.lms.api.entities.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LibraryRepo extends JpaRepository<Library, Long> {
+    @Query("SELECT l from Library l INNER JOIN books b ON b.id = :bookId")
+    Library findLibraryByBookId(@Param("bookId") Long bookId);
 }
